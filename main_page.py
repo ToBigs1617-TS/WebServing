c8974eb65ee4b776e5bc4673f043d0dab1cb9483
hanna <hannapark56@sookmyung.ac.kr>
2022-07-03 16:25:53 +0900

    streamlit multipage 구현

---
+++ main_page.py
@@ -0,0 +1,11 @@
+# Contents of ~/my_app/main_page.py
+import streamlit as st
+from PIL import Image
+
+st.title("Main page")
+st.sidebar.markdown("# Main page")
+st.markdown("## Project demo")
+
+image = Image.open('sample.jpg')
+
+st.image(image, caption='샘플 이미지..')
